e0eef6d88ef2a9b6a3e60d654505bf1370b8e886
toyo <tyudosen@gmail.com>
2025-08-08 03:40:23 -0500

    fix import

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import { HttpApi } from "@effect/platform";
-import { DeepResearchApiGroup } from "./deep-research";
+import { DeepResearchApiGroup } from "./deep-research.js";
 
 export class Api extends HttpApi.make("DeepResearchApi")
 	.add(DeepResearchApiGroup)
